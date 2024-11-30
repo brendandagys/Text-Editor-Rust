b8aee3dfc75b29ffd06bd825837d135a66e76746
Brendan Dagys <brendandagys@gmail.com>
2024-11-30 16:04:44 -0500

    Refactor

--- text-editor/src/main.rs
+++ text-editor/src/main.rs
@@ -1,11 +1,13 @@
 use std::error::Error;
-use std::io::{self, Read};
+use std::io;
 use std::os::unix::io::AsRawFd;
 
 use editor_instance::EditorInstance;
-use utils::{debug_input, panic_with_error, set_panic_hook};
+use input::process_keypress;
+use utils::set_panic_hook;
 
 mod editor_instance;
+mod input;
 mod terminal;
 mod utils;
 
@@ -17,18 +19,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     set_panic_hook(stdin_fd);
 
     let active_editor = EditorInstance::new(stdin_fd);
-    let mut byte_buffer = [0u8; 1];
 
     loop {
-        match stdin_lock.read_exact(&mut byte_buffer) {
-            Ok(_) => {
-                let key = byte_buffer[0];
-                debug_input(key);
-
-                active_editor.process_key(key);
-            }
-            Err(e) if e.kind() == std::io::ErrorKind::UnexpectedEof => {}
-            Err(e) => panic_with_error(e, "Error reading byte into buffer"),
-        };
+        process_keypress(&mut stdin_lock, &active_editor);
     }
 }
