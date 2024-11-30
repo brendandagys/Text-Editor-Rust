1bc77e7676ec78dce0910a0d4fb994abb45ed537
Brendan Dagys <brendandagys@gmail.com>
2024-11-30 14:19:40 -0500

    Improve error reporting framework

--- text-editor/src/terminal.rs
+++ text-editor/src/terminal.rs
@@ -1,29 +1,70 @@
-use std::io;
-use termios::{tcgetattr, tcsetattr, Termios, ECHO, ICANON, TCSAFLUSH};
+use termios::{
+    tcgetattr, tcsetattr, Termios, BRKINT, CS8, ECHO, ICANON, ICRNL, IEXTEN, INPCK, ISIG, ISTRIP,
+    IXON, OPOST, TCSAFLUSH, VMIN, VTIME,
+};
 
-pub fn get_populated_termios(stdin_fd: i32) -> io::Result<Termios> {
-    let mut termios = Termios::from_fd(stdin_fd)?;
-    tcgetattr(stdin_fd, &mut termios)?;
-    Ok(termios)
+use crate::utils::panic_with_error;
+
+pub fn get_populated_termios(stdin_fd: i32) -> Termios {
+    let mut termios = match Termios::from_fd(stdin_fd) {
+        Ok(termios) => termios,
+        Err(e) => panic_with_error(e, "Unable to get current terminal configuration"),
+    };
+
+    match tcgetattr(stdin_fd, &mut termios) {
+        Ok(_) => termios,
+        Err(e) => panic_with_error(
+            e,
+            "Unable to populate new `termios` object with current terminal configuration",
+        ),
+    }
 }
 
-pub fn disable_raw_mode(stdin_fd: i32, original_termios: Termios) -> io::Result<()> {
-    tcsetattr(stdin_fd, TCSAFLUSH, &original_termios)
+pub fn disable_raw_mode(stdin_fd: i32, original_termios: Termios) -> () {
+    match tcsetattr(stdin_fd, TCSAFLUSH, &original_termios) {
+        Ok(_) => (),
+        Err(e) => panic_with_error(e, "Unable to reset terminal settings (disable raw mode)"),
+    }
 }
 
-pub fn enable_raw_mode(stdin_fd: i32) -> io::Result<()> {
-    let mut raw_termios = get_populated_termios(stdin_fd)?;
+pub fn enable_raw_mode(stdin_fd: i32) -> () {
+    let mut raw_termios = get_populated_termios(stdin_fd);
 
-    // `c_lflag`: local/miscellaneous flags
-    raw_termios.c_lflag &= !(ECHO | ICANON);
+    // `c_lflag`: LOCAL/MISCELLANEOUS FLAGS
+    raw_termios.c_lflag &= !(
+        ECHO    // Disable echoing keys to terminal
+      | ICANON  // Disable Canonical mode (press Enter to submit)
+      | IEXTEN  // Fix for possible Ctrl-V wait-for-next-character issue
+      | ISIG
+        // `SIGINT` (Ctrl-C) AND `SIGTSTP` (Ctrl-Z) which terminate and suspend, respectively
+    );
 
-    // `c_iflag`: Input flags
+    // `c_iflag`: INPUT FLAGS
 
-    // `c_oflag`: Output flags
+    raw_termios.c_iflag &= !(
+        IXON // No software flow control (pause/resume transmission) [Ctrl-S and Ctrl-Q]
+      | BRKINT // Disable break condition causing a `SIGINT` (e.g. Ctrl-C)
+      | INPCK // Disable input parity checking
+      | ISTRIP // Strip 8th bit of each input byte (set to 0)
+      | ICRNL
+        // Carriage return (13) -> new line (affects Ctrl-M [13])
+    );
 
-    // `c_cflag`: Control flags
+    // `c_oflag`: OUTPUT FLAGS
+    raw_termios.c_oflag &= !(
+        // Disable output post-processing: "\n" -> "\r\n"
+        OPOST
+    );
 
-    tcsetattr(stdin_fd, TCSAFLUSH, &raw_termios)?;
+    // `c_cflag`: CONTROL FLAGS
+    raw_termios.c_cflag |= CS8; // Set character size to 8 bits/byte
 
-    Ok(())
+    // Control characters (array of bytes controlling various terminal settings)
+    raw_termios.c_cc[VMIN] = 0; // Minimum bytes needed before `read()` returns
+    raw_termios.c_cc[VTIME] = 1; // Time-out (1/10 second)
+
+    match tcsetattr(stdin_fd, TCSAFLUSH, &raw_termios) {
+        Ok(_) => (),
+        Err(e) => panic_with_error(e, "Unable to set terminal settings (enable raw mode)"),
+    }
 }
