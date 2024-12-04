4b7bc2929ef32f811f923382bee613bc1ed97c0f
Brendan Dagys <bdagys@rentsync.com>
2024-12-04 16:23:52 -0500

    Spawn thread to watch for terminal window size change Update row/column count in active editor on change

--- text-editor/src/main.rs
+++ text-editor/src/main.rs
@@ -3,8 +3,9 @@ use input::process_keypress;
 use output::refresh_screen;
 use std::error::Error;
 use std::io;
+use std::sync::{Arc, RwLock};
 use terminal::{enable_raw_mode, get_populated_termios};
-use utils::set_panic_hook;
+use utils::{set_panic_hook, watch_for_window_size_change};
 
 mod editor_instance;
 mod globals;
@@ -14,17 +15,32 @@ mod terminal;
 mod utils;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut stdin_lock = io::stdin().lock();
+    let stdin = io::stdin();
 
     let termios = get_populated_termios();
 
     set_panic_hook(termios);
     enable_raw_mode(termios);
 
-    let active_editor = EditorInstance::new(termios, &mut stdin_lock);
+    let active_editor = Arc::new(RwLock::new(EditorInstance::new(termios, &mut stdin.lock())));
+
+    watch_for_window_size_change(Arc::clone(&active_editor));
 
     loop {
-        refresh_screen(active_editor.screen_rows_columns.0);
-        process_keypress(&mut stdin_lock, &active_editor);
+        let screen_rows_columns = {
+            active_editor
+                .read()
+                .expect("Could not get reader for editor")
+                .screen_rows_columns
+        };
+
+        refresh_screen(screen_rows_columns.0);
+
+        process_keypress(
+            &mut stdin.lock(),
+            &active_editor
+                .read()
+                .expect("Could not get reader for editor"),
+        );
     }
 }
