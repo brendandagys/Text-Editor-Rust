05317f676f3ade470319b9e319b5ae0ff461fc21
Brendan Dagys <brendandagys@gmail.com>
2024-12-07 21:39:23 -0500

    Refactor detection/update of window size Move `draw_rows()` and `move_cursor_to_position()` into `EditorInstance` as methods

--- text-editor/src/main.rs
+++ text-editor/src/main.rs
@@ -4,7 +4,7 @@ use output::{clear_display, refresh_screen};
 use std::error::Error;
 use std::sync::{Arc, RwLock};
 use terminal::{enable_raw_mode, get_populated_termios};
-use utils::{set_panic_hook, watch_for_window_size_change};
+use utils::{get_window_size, set_panic_hook, watch_for_window_size_change};
 
 mod editor_instance;
 mod globals;
@@ -13,6 +13,12 @@ mod output;
 mod terminal;
 mod utils;
 
+#[derive(Clone, Copy)]
+pub struct WindowSize {
+    pub rows: u16,
+    pub columns: u16,
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     clear_display(); // Does not rely on raw mode
 
@@ -20,26 +26,18 @@ fn main() -> Result<(), Box<dyn Error>> {
     set_panic_hook(termios);
     enable_raw_mode(termios);
 
-    let active_editor = Arc::new(RwLock::new(EditorInstance::new(termios)));
-    watch_for_window_size_change(Arc::clone(&active_editor));
+    let window_size = Arc::new(RwLock::new(get_window_size()));
+    watch_for_window_size_change(Arc::clone(&window_size));
+
+    let mut active_editor = EditorInstance::new(termios);
 
     loop {
-        let EditorInstance {
-            window_size,
-            cursor_position,
-            ..
-        } = *{
-            active_editor
-                .read()
-                .expect("Could not get reader for editor")
-        };
+        let window_size = *window_size
+            .read()
+            .expect("Could not get window size read lock");
 
-        refresh_screen(window_size, cursor_position);
+        refresh_screen(&active_editor, window_size);
 
-        process_keypress(
-            &mut active_editor
-                .write()
-                .expect("Could not get reader for editor"),
-        );
+        process_keypress(&mut active_editor);
     }
 }
