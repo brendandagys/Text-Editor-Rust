fcbdf32e25664029a27131de2d0b154738431dfa
Brendan Dagys <brendandagys@gmail.com>
2024-12-20 05:43:59 -0500

    Highlight floats (numbers with a decimal point)

--- text-editor/src/editor_instance.rs
+++ text-editor/src/editor_instance.rs
@@ -151,6 +151,7 @@ impl EditorInstance {
 
             if char.is_ascii_digit()
                 && (is_previous_char_separator || previous_highlight == &HighlightType::Number)
+                || (char == '.' && previous_highlight == &HighlightType::Number)
             {
                 highlight[i] = HighlightType::Number;
                 i += 1;
