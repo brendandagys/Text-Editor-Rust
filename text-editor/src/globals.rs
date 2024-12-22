403bbac2266b7e74a4734a28966a15fbf43bea5c
Brendan Dagys <brendandagys@gmail.com>
2024-12-22 10:54:17 -0500

    Highlight keywords

--- text-editor/src/globals.rs
+++ text-editor/src/globals.rs
@@ -16,6 +16,8 @@ pub fn get_buffer_lock() -> MutexGuard<'static, [u8; 1]> {
 pub struct Syntax {
     pub file_type: &'static str,
     pub file_match: &'static [&'static str],
+    pub keywords: &'static [&'static str],
+    pub types: &'static [&'static str],
     pub single_line_comment_start: &'static str,
     pub flags: i32,
 }
@@ -26,6 +28,13 @@ pub const HIGHLIGHT_STRINGS: i32 = 1 << 1;
 pub static SYNTAX_CONFIGURATIONS: &'static [Syntax] = &[Syntax {
     file_type: "C",
     file_match: &[".c", ".h", ".cpp"],
+    keywords: &[
+        "switch", "if", "while", "for", "break", "continue", "return", "else", "struct", "union",
+        "typedef", "static", "enum", "class", "case",
+    ],
+    types: &[
+        "int", "long", "double", "float", "char", "unsigned", "signed", "void",
+    ],
     single_line_comment_start: "//",
     flags: HIGHLIGHT_NUMBERS | HIGHLIGHT_STRINGS,
 }];
