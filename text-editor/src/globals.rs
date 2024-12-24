6971fb7875a0326af23f27823d7e10189cae5c30
Brendan Dagys <brendandagys@gmail.com>
2024-12-24 12:05:51 -0500

    Add syntax highlighting for Rust

--- text-editor/src/globals.rs
+++ text-editor/src/globals.rs
@@ -27,18 +27,38 @@ pub struct Syntax {
 pub const HIGHLIGHT_NUMBERS: i32 = 1 << 0;
 pub const HIGHLIGHT_STRINGS: i32 = 1 << 1;
 
-pub static SYNTAX_CONFIGURATIONS: &'static [Syntax] = &[Syntax {
-    file_type: "C",
-    file_match: &[".c", ".h", ".cpp"],
-    keywords: &[
-        "switch", "if", "while", "for", "break", "continue", "return", "else", "struct", "union",
-        "typedef", "static", "enum", "class", "case",
-    ],
-    types: &[
-        "int", "long", "double", "float", "char", "unsigned", "signed", "void",
-    ],
-    single_line_comment_start: "//",
-    multi_line_comment_start: "/*",
-    multi_line_comment_end: "*/",
-    flags: HIGHLIGHT_NUMBERS | HIGHLIGHT_STRINGS,
-}];
+pub static SYNTAX_CONFIGURATIONS: &'static [Syntax] = &[
+    Syntax {
+        file_type: "C",
+        file_match: &[".c", ".h", ".cpp"],
+        keywords: &[
+            "switch", "if", "while", "for", "break", "continue", "return", "else", "struct",
+            "union", "typedef", "static", "enum", "class", "case",
+        ],
+        types: &[
+            "int", "long", "double", "float", "char", "unsigned", "signed", "void",
+        ],
+        single_line_comment_start: "//",
+        multi_line_comment_start: "/*",
+        multi_line_comment_end: "*/",
+        flags: HIGHLIGHT_NUMBERS | HIGHLIGHT_STRINGS,
+    },
+    Syntax {
+        file_type: "Rust",
+        file_match: &[".rs"],
+        keywords: &[
+            "as", "break", "const", "continue", "crate", "else", "enum", "extern", "false", "fn",
+            "for", "if", "impl", "in", "let", "loop", "match", "mod", "move", "mut", "pub", "ref",
+            "return", "self", "static", "struct", "super", "trait", "true", "type", "unsafe",
+            "use", "where", "while",
+        ],
+        types: &[
+            "bool", "char", "f32", "f64", "i8", "i16", "i32", "i64", "i128", "isize", "str", "u8",
+            "u16", "u32", "u64", "u128", "usize",
+        ],
+        single_line_comment_start: "//",
+        multi_line_comment_start: "/*",
+        multi_line_comment_end: "*/",
+        flags: HIGHLIGHT_NUMBERS | HIGHLIGHT_STRINGS,
+    },
+];
